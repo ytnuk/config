c72b2fb93abac76898b916160266d053a80a9f65
Vít Kutný <vitkutny@icloud.com>
2016-01-02 19:35:57 +0100

    code style: do not wrap call parameters

--- src/Config.php
+++ src/Config.php
@@ -10,64 +10,29 @@ final class Config
 	public static function dump(Composer\Script\Event $event)
 	{
 		$composer = $event->getComposer();
-		Composer\Autoload\includeFile(
-			implode(
-				DIRECTORY_SEPARATOR,
-				[
-					$vendorDir = $composer->getConfig()->get('vendor-dir'),
-					'autoload.php',
-				]
-			)
-		);
+		Composer\Autoload\includeFile(implode(DIRECTORY_SEPARATOR, [
+			$vendorDir = $composer->getConfig()->get('vendor-dir'),
+			'autoload.php',
+		]));
 		$repository = $composer->getRepositoryManager()->getLocalRepository();
 		$config = [
 			'extensions' => [],
 		];
 		foreach ($repository->getPackages() as $package) {
-			$extra = array_filter(
-				$package->getExtra(),
-				'is_array'
-			);
+			$extra = array_filter($package->getExtra(), 'is_array');
 			if (isset($extra['extensions'])) {
-				$config['extensions'] += $extensions = array_filter(
-					array_map(
-						[
-							Nette\Neon\Neon::class,
-							'decode',
-						],
-						array_filter(
-							$extra['extensions'],
-							'is_string'
-						)
-					),
-					function ($class) {
-						return is_subclass_of(
-							$class instanceof Nette\Neon\Entity ? $class->value : $class,
-							Nette\DI\CompilerExtension::class
-						);
-					}
-				);
-				$config = array_merge_recursive(
-					$config,
-					array_intersect_key(
-						$extra,
-						$extensions
-					)
-				);
+				$config['extensions'] += $extensions = array_filter(array_map([
+					Nette\Neon\Neon::class,
+					'decode',
+				], array_filter($extra['extensions'], 'is_string')), function ($class) {
+					return is_subclass_of($class instanceof Nette\Neon\Entity ? $class->value : $class, Nette\DI\CompilerExtension::class);
+				});
+				$config = array_merge_recursive($config, array_intersect_key($extra, $extensions));
 			}
 		}
-		file_put_contents(
-			implode(
-				DIRECTORY_SEPARATOR,
-				[
-					$vendorDir,
-					'config.neon',
-				]
-			),
-			Nette\Neon\Neon::encode(
-				$config,
-				Nette\Neon\Neon::BLOCK
-			)
-		);
+		file_put_contents(implode(DIRECTORY_SEPARATOR, [
+			$vendorDir,
+			'config.neon',
+		]), Nette\Neon\Neon::encode($config, Nette\Neon\Neon::BLOCK));
 	}
 }
